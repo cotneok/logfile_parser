b2aca7d91bb1560200e70a446a523dfcc143dda2
cotneok <cotne.ok@gmail.com>
2021-03-15 00:19:51 +0400

    Add logic to read file and parse entries

--- lib/logfile_parser.rb
+++ lib/logfile_parser.rb
@@ -9,11 +9,17 @@ class LogfileParser
   end
 
   def self.read_file(file_path)
-
+    parser = LogfileParser.new
+    File.readlines(file_path).each do |line|
+      parser.entries << parse_entry(line)
+    end
+    parser
   end
 
   def self.parse_entry(string)
-
+    path, ip = string.split
+    ActivityLog.new(path, ip)
   end
 
+  ActivityLog = Struct.new(:path, :ip)
 end
